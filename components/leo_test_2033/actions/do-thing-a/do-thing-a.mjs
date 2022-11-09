2eddedde14fcb3445fd121b5a10b5d6cfd22ba88
Hung Vu <vunguyenhung@outlook.com>
2022-11-09 21:17:39 +0700

    Add app prop

--- components/leo_test_2033/actions/do-thing-a/do-thing-a.mjs
+++ components/leo_test_2033/actions/do-thing-a/do-thing-a.mjs
@@ -1,9 +1,13 @@
+import app from "../../leo_test_2033.app.mjs";
+
 export default {
   name: "Do Thing A",
   version: "0.0.1",
   key: "leo_test_2033-do-thing-a",
   description: "",
-  props: {},
+  props: {
+    app,
+  },
   type: "action",
   methods: {},
   async run({ $ }) {
